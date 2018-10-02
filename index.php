a40464fc8312f57da2761a99a8e6d7256c948d74
bastiensabatier <bastien@agence-hybrid.com>
2018-10-02 12:20:23 +0200

    add index.php test file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr"> 
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, user-scalable=no" />
+		<title>test</title>
+	</head>
+	<body>
+		<main>
+			<span>test</span>
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
